5df1acc141aa6d6953fb9a7ecc7021b23bb14ac9
jaonoctus <jaonoctus@protonmail.com>
2023-03-01 20:51:30 -0300

    feat: initial content

--- app.config.ts
+++ app.config.ts
@@ -1,26 +1,44 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
+    title: 'Learn me a Bitcoin',
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxtstudio',
-      github: 'nuxt-themes/docus'
+      github: 'jaonoctus/learnmeabitcoin'
     },
     aside: {
-      level: 0,
-      exclude: []
+      level: 1,
     },
     header: {
-      logo: true
+      logo: false,
     },
     footer: {
+      textLinks: [
+        {
+          text: 'João Dias (@jaonoctus)',
+          href: 'https://jaonoct.us'
+        },
+        {
+          text: '/',
+          href: ''
+        },
+        {
+          text: 'Greg Walker (@in3rsha)',
+          href: 'https://learnmeabitcoin.com'
+        }
+      ],
       iconLinks: [
         {
           href: 'https://nuxt.com',
           icon: 'simple-icons:nuxtdotjs'
         }
       ]
+    },
+    github: {
+      owner: 'jaonoctus',
+      repo: 'learnmeabitcoin',
+      branch: 'main',
+      edit: true
     }
   }
 })
