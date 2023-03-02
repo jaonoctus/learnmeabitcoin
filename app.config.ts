cb3eafb938f2bd905b60bdc6286961193cc947ca
jaonoctus <jaonoctus@protonmail.com>
2023-03-02 00:27:39 -0300

    fix: update page head attrs

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   docus: {
     title: 'Learn me a Bitcoin',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    description: 'How does Bitcoin Work?',
+    image: '',
     socials: {
       github: 'jaonoctus/learnmeabitcoin'
     },
