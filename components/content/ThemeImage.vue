c1b9d62468981e32bd3d4fa7abf8046ec9233f30
jaonoctus <jaonoctus@protonmail.com>
2023-03-02 00:22:24 -0300

    feat: theme image

---
+++ components/content/ThemeImage.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps(['name', 'ext', 'alt'])
+const colorMode = useColorMode()
+
+const src = ref('')
+
+onMounted(() => {
+  console.log('init')
+  updateImageSrc()
+})
+
+watch(colorMode, () => {
+  console.log('changed')
+  updateImageSrc()
+})
+
+function getTheme () {
+  return colorMode.value
+}
+
+function updateImageSrc () {
+  const theme = getTheme()
+  console.log(theme)
+  src.value = `${props.name}_${theme}.${props.ext}`
+}
+</script>
+
+<template>
+  <p>
+    <img :src="src" :alt="alt"/>
+  </p>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,8 @@
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus'
+  extends: '@nuxt-themes/docus',
+  // @ts-ignore
+  colorMode: {
+    preference: 'dark',
+    fallback: 'dark'
+  }
 })
